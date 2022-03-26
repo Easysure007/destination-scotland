99701c1a08aa5dd8f9cbe3346c9f88206c5d2389
Easysure007 <opeyemi_isijola@yahoo.com>
2022-03-26 02:25:49 +0100

    added controllers

--- app/Http/Controllers/DestinationCommentController.php
+++ app/Http/Controllers/DestinationCommentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DestinationController extends Controller
+class DestinationCommentController extends Controller
 {
     /**
      * Display a listing of the resource.
